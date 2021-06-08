3ee4e480b54d9309a135565ac48cdefa8da9cfa9
Cristal Zavala <czavala7133@southwestuniversity.edu>
2021-06-08 10:55:57 -0600

    this is module 3 and 4

--- MyGramHM/MyGramHM/Controllers/HomeController.cs
+++ MyGramHM/MyGramHM/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace MyGramHM.Controllers
         {
             return View();
         }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
